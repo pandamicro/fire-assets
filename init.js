a9f13b5c9f0211b775f5bb46001ecbbb0d76f1fa
Johnny Wu <KarasAya@gmail.com>
2015-07-03 08:18:47 +0800

    prevent runtime loading meta file

--- init.js
+++ init.js
@@ -1,45 +1,50 @@
 // register asset, folder and mount
 // =======================================
 
-Editor.metas.mount = {
-    'meta-type': 'mount',
-    'meta-icon': 'packages://canvas-assets/static/mount.png',
-};
+if ( !Editor.isRuntime ) {
+    Editor.metas.mount = {
+        'meta-type': 'mount',
+        'meta-icon': 'packages://canvas-assets/static/mount.png',
+    };
 
-// NOTE: AssetMeta defines 'meta-type' in asset-db
-// Editor.metas.asset['meta-type'] = 'asset';
-Editor.metas.asset['meta-icon'] = 'packages://canvas-assets/static/asset.png';
+    // NOTE: AssetMeta defines 'meta-type' in asset-db
+    // Editor.metas.asset['meta-type'] = 'asset';
+    Editor.metas.asset['meta-icon'] = 'packages://canvas-assets/static/asset.png';
 
-// NOTE: FolderMeta defines 'meta-type' in asset-db
-// Editor.metas.folder['meta-type'] = 'folder';
-Editor.metas.folder['meta-icon'] = 'packages://canvas-assets/static/folder.png';
+    // NOTE: FolderMeta defines 'meta-type' in asset-db
+    // Editor.metas.folder['meta-type'] = 'folder';
+    Editor.metas.folder['meta-icon'] = 'packages://canvas-assets/static/folder.png';
 
-if ( Editor.isPageLevel ) {
-    Editor.inspectors.asset = 'packages://canvas-assets/inspector/asset.html';
-    Editor.inspectors.folder = 'packages://canvas-assets/inspector/folder.html';
-    Editor.inspectors.mount = 'packages://canvas-assets/inspector/mount.html';
+    if ( Editor.isPageLevel ) {
+        Editor.inspectors.asset = 'packages://canvas-assets/inspector/asset.html';
+        Editor.inspectors.folder = 'packages://canvas-assets/inspector/folder.html';
+        Editor.inspectors.mount = 'packages://canvas-assets/inspector/mount.html';
+    }
 }
 
 // register builtin asset
 // =======================================
+
 [
     'texture',
     'sprite',
     'bitmap-font',
     'ttf-font'
 ].forEach( function ( name ) {
-    // meta
-    Editor.metas[name] = require('./meta/' + name);
-    Editor.metas[name]['meta-type'] = name;
-    Editor.metas[name]['meta-icon'] = 'packages://canvas-assets/static/' + name + '.png';
+    if ( !Editor.isRuntime ) {
+        // meta
+        Editor.metas[name] = require('./meta/' + name);
+        Editor.metas[name]['meta-type'] = name;
+        Editor.metas[name]['meta-icon'] = 'packages://canvas-assets/static/' + name + '.png';
 
-    // inspector
-    if ( Editor.isPageLevel ) {
-        Editor.inspectors[name] = 'packages://canvas-assets/inspector/' + name + '.html';
+        // inspector
+        if ( Editor.isPageLevel ) {
+            Editor.inspectors[name] = 'packages://canvas-assets/inspector/' + name + '.html';
+        }
     }
 
-    // asset
     if ( Editor.isCoreLevel || Editor.isRuntime ) {
+        // asset
         require('./asset/' + name);
     }
 });
