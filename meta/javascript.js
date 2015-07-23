9519b7e6bd32217e823ae43268c518702ce6e5b9
Johnny Wu <KarasAya@gmail.com>
2015-07-23 15:56:28 +0800

    use babel compile javascript

--- meta/javascript.js
+++ meta/javascript.js
@@ -18,17 +18,32 @@ JavaScriptMeta.prototype.deserialize = function ( jsonObj ) {
 JavaScriptMeta.prototype.import = function ( assetdb, fspath, cb ) {
     var Async = require('async');
     var Path = require('fire-path');
+    var Babel = require('babel');
+
     var self = this;
 
     Async.waterfall([
         function ( next ) {
+            var data;
+            try {
+                var str = Fs.readFileSync( fspath, {encoding: 'utf-8'} );
+                result = Babel.transform(str, {
+                    ast: false,
+                });
+                next ( null, result.code );
+            } catch (err) {
+                next ( err );
+            }
+        },
+
+        function ( data, next ) {
             var basename = Path.basename(fspath);
 
             var asset = new Fire.JavaScript();
             asset.name = Path.basenameNoExt(fspath);
             asset._setRawFiles([basename]);
 
-            assetdb.copyRawfileToLibrary( self.uuid, fspath );
+            assetdb.saveRawdataToLibrary( self.uuid, data, basename );
             assetdb.saveAssetToLibrary( self.uuid, asset );
 
             next ( null );
