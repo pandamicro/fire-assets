6a867837b074b8b9b250db86367c96ac884c3791
Johnny Wu <KarasAya@gmail.com>
2015-08-03 12:10:53 +0800

    change meta-icon to asset-icon

--- init.js
+++ init.js
@@ -4,16 +4,16 @@
 if ( !Editor.isRuntime ) {
     Editor.metas.mount = {
         'asset-type': 'mount',
-        'meta-icon': 'packages://canvas-assets/static/icon/mount.png',
+        'asset-icon': 'packages://canvas-assets/static/icon/mount.png',
     };
 
     // NOTE: AssetMeta defines 'asset-type' in asset-db
     // Editor.metas.asset['asset-type'] = 'asset';
-    Editor.metas.asset['meta-icon'] = 'packages://canvas-assets/static/icon/asset.png';
+    Editor.metas.asset['asset-icon'] = 'packages://canvas-assets/static/icon/asset.png';
 
     // NOTE: FolderMeta defines 'asset-type' in asset-db
     // Editor.metas.folder['asset-type'] = 'folder';
-    Editor.metas.folder['meta-icon'] = 'packages://canvas-assets/static/icon/folder.png';
+    Editor.metas.folder['asset-icon'] = 'packages://canvas-assets/static/icon/folder.png';
 
     if ( Editor.isPageLevel ) {
         Editor.inspectors.asset = 'packages://canvas-assets/inspector/asset.html';
@@ -30,7 +30,7 @@ require('./asset').forEach( function ( name ) {
         // meta
         Editor.metas[name] = require('./meta/' + name);
         Editor.metas[name]['asset-type'] = name;
-        Editor.metas[name]['meta-icon'] = 'packages://canvas-assets/static/icon/' + name + '.png';
+        Editor.metas[name]['asset-icon'] = 'packages://canvas-assets/static/icon/' + name + '.png';
 
         // inspector
         if ( Editor.isPageLevel ) {
